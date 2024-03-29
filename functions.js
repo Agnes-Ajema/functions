a7b1809001b2ce1226e70188a3a032afa98a93eb
Agnes <agnesajema@gmail.com>
2024-03-29 11:37:33 +0300

    add files

---
+++ functions.js
@@ -0,0 +1,24 @@
+let arr =["Gian","Mary","Agnes","Vivian"];
+
+function arrSort(arr) {
+  arr.sort() ;
+  return arr;
+   
+}
+console.log(arrSort(arr))
+
+function arrReverse(arr) {
+arr.reverse();
+return arr;
+    
+}
+console.log(arrReverse(arr));
+
+let arr1 = [2,3,4,5]
+function multiply(arr1) {
+ var product = 1;
+ for (var i = 0;i<n;i++)   {
+    product *= arr1[i];
+ }
+ console.log(product);
+}
